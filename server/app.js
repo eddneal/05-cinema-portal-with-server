ac26d3b3c2cfb56e34c3fad75e892ef63b42b8c0
Jaroslav Kubicek <jaroslav.kubicek@merck.com>
2017-04-05 16:47:34 +0200

    orders api added

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const low = require('lowdb');
 const fileAsync = require('lowdb/lib/storages/file-async');
 const movies = require('./data/movies');
+const users = require('./data/users');
 const cookieSession = require('cookie-session');
 
 const app = express();
@@ -16,8 +17,7 @@ const isLoggedIn = (req, res, next) => {
     return next();
   }
 
-  next(new Error('User is not logged in'));
-  // TODO
+  next(new Error('User is not logged in.'));
 };
 
 app.use(morgan('dev'));
@@ -33,7 +33,26 @@ app.use(bodyParser.json());
 
 
 app.post('/login', function (req, res) {
-  // TODO
+  const data = req.body;
+
+  if (data.username && data.password) {
+    const user = db
+      .get('users')
+      .find((u) => u.username === data.username && u.password === data.password)
+      .value();
+
+    if (!user) {
+      return res.status(401).send({error: {message: 'Bad credentials'}})
+    }
+
+    req.session.user = {
+      username: data.username
+    };
+    return res.send({ok: true});
+  }
+
+  const message = 'Bad request - you need to provide username & password';
+  res.status(400).send({error: {message}});
 });
 
 app.get('/movies', function (req, res, next) {
@@ -58,6 +77,82 @@ app.get('/movies/:id', (req, res) => {
   setTimeout(() => res.send(movie), 1000);
 });
 
+app.get('/orders', isLoggedIn, (req, res) => {
+  const userOrders = db
+    .get(['orders', req.session.user.username])
+    .value();
+
+  res.send(userOrders || []);
+});
+
+app.post('/orders', isLoggedIn, function (req, res, next) {
+  const username = req.session.user.username;
+  const data = req.body;
+
+  if (!(data.movieId && data.noOfTickets)) {
+    const message = 'Bad request - you need to provide "movieId" & "noOfTickets"';
+
+    return res.status(400).send({error: {message}});
+  }
+
+  const movie = db.get('movies')
+    .find({id: +data.movieId})
+    .value();
+
+  if (!movie) {
+    const message = 'Bad request - invalid "movieId"';
+
+    return res.status(400).send({error: {message}});
+  }
+
+  if (data.noOfTickets > movie.availableSeats) {
+    const message = 'Bad request - not enough seats.';
+
+    return res.status(400).send({error: {message}});
+  }
+
+  const index = db.get('movies')
+    .findIndex({id: +data.movieId});
+  const availableSeats = movie.availableSeats - data.noOfTickets;
+  const order = {
+    id: Math.random().toString(16).substr(2),
+    movieId: movie.id,
+    noOfTickets: data.noOfTickets
+  };
+
+  db
+    .set(['movies', index], Object.assign({}, movie, {availableSeats}))
+    .update(['orders', username], (orders) => orders ? orders.concat(order) : [order])
+    .write()
+    .then(() => res.send(order), next);
+});
+
+app.delete('/orders/:id', isLoggedIn, function (req, res, next) {
+  const username = req.session.user.username;
+  const id = req.params.id;
+
+  const order = db.get(['orders', username]).find({id}).value();
+
+  if (!order) {
+    return res
+      .status(400)
+      .send({error: {message: 'Trying to delete non-existing order'}})
+  }
+
+  const index = db.get('movies')
+    .findIndex({id: order.movieId});
+
+  db
+    .update(
+      ['movies', index],
+      (movie) => Object.assign({}, movie, {availableSeats: movie.availableSeats + order.noOfTickets})
+    )
+    .get(['orders', username])
+    .remove(order => order.id === id)
+    .write()
+    .then(() => res.send({ok: true}),next);
+});
+
 app.use(function (err, req, res, next) {
   res
     .status(500)
@@ -69,7 +164,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
-db.defaults({movies})
+db.defaults({movies, orders: {}, users})
   .write()
   .then(() => {
     app.listen(5000, () => console.log('Server is listening'))
